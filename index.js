6f2b88d619f9307c73f3e4650a680ae1523eab9f
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2024-03-04 11:12:34 -0300

    atualização

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express')
 
 app = express()
 
+const PORT = 3333
+
 app.get('/', (req, res) => {
 return res.status(200).json({message: 'SERVER ESCUTANDO'})
 })
 
-app.listen(3333)
+app.listen(PORT, ()=>{
+    console.log('SERVER LIGADO')
+})
