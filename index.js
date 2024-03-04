1e4c20cb95cd37410df6d5b45543091cdd74237d
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2024-03-04 11:26:03 -0300

    atualização

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app = express()
 const PORT = 3333
 
 app.get('/', (req, res) => {
-return res.status(200).json({message: 'SERVER ESCUTANDO'})
+return res.status(200).json({message: 'Cecilia De Lana'})
 })
 
 app.listen(PORT, ()=>{
